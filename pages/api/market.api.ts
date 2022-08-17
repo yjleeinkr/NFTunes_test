4feaf36ac064a42ed435ddbb7c0d2469d27e7d9c
yoon <yoonk1228@naver.com>
2022-08-17 12:05:45 +0900

    test: todo - markets api 리턴값을 state 에 저장

--- pages/api/market.api.ts
+++ pages/api/market.api.ts
@@ -10,6 +10,15 @@ export async function getMarkets() {
   };
 }
 
+// import type { NextApiHandler } from "next";
+//
+// const getMarketsHandler: NextApiHandler = async (req,res) => {
+//
+//   res.json
+// }
+//
+// export default getMarketsHandler
+
 export function getOne( param:string ):string {
   console.log('getOne: ',param)
   return(
