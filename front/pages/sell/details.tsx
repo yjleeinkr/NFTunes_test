17563f8dd34a568d4e35082b4e94b18c708df06d
bbb4756 <rlarbfl4756@gmail.com>
2022-08-25 18:16:21 +0900

    확인

--- front/pages/sell/details.tsx
+++ front/pages/sell/details.tsx
@@ -3,8 +3,37 @@ import { NextPage } from 'next';
 
 // Work: gyuri
 // last update: 220825
-const OutBox = styled.div``;
 
-const Details = () => {};
+const OutBox = styled.div`
+  background: black;
+  width: 100%;
+  height: 1400px;
+  padding-top: 100px;
+`;
 
-export default Details;
+const MainBox = styled.div`
+  width: 70%;
+  height: 1200px;
+  background: red;
+  margin: 0 auto;
+`;
+
+const MusicInfoBox = styled.div`
+  width: 100%;
+  height: 600px;
+  background: pink;
+`;
+
+const DetailsPage: NextPage = () => {
+  return (
+    <>
+      <OutBox>
+        <MainBox>
+          <MusicInfoBox></MusicInfoBox>
+        </MainBox>
+      </OutBox>
+    </>
+  );
+};
+
+export default DetailsPage;
