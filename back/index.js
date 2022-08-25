c8aa0b10f7cba6a5cd0f111c5243fe8099717649
yjleeinkr <lucy.lee930216@gmail.com>
2022-08-25 10:35:05 +0900

    [back/db] changed db setting to connect w.mongodb atlas

--- back/index.js
+++ back/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
 const router = require('./routes/index');
+const mongodb = require('mongodb');
 
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/nftMarket';
+const MONGODB_PW = process.env.MONGODB_PW;
+
+const MongoClient = mongodb.MongoClient;
+const mongoUrl = `mongodb+srv://team1:${MONGODB_PW}@nftmarket.skj99nw.mongodb.net/nftMarket?retryWrites=true&w=majority`;
 
 app.use(
   cors({
@@ -18,11 +22,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', router);
 
-app.listen(PORT, async () => {
-  try {
-    await mongoose.connect(MONGODB_URI);
-    console.log(`connected to mongoDB, server's running on ${PORT} 🚀`);
-  } catch (error) {
-    console.log(error);
-  }
-});
+MongoClient.connect(mongoUrl)
+  .then((client) => {
+    console.log('mongo connected');
+    console.log(client);
+  })
+  .then(
+    app.listen(PORT, () => {
+      console.log(`connected to mongoDB, server's running on ${PORT} 🚀`);
+    }),
+  )
+  .catch((err) => console.log(err));
