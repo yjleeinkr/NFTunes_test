572f089d287fe5afbd697d7d42f810265b9f38dd
o-haa <haeun9627@hanmail.net>
2022-08-30 12:34:15 +0900

    pull

--- truffle/contracts/Subscribe_yj.sol
+++ truffle/contracts/Subscribe_yj.sol
@@ -3,7 +3,10 @@
 pragma solidity ^0.8.15;
 
 contract Subscribe_yj{
-    uint256 public sub_price = 1 ether;
+    uint256 public sub_price1 = 1 ether;
+    uint256 public sub_price3 = 2.5 ether;
+    uint256 public sub_price6 = 5 ether;
+
     mapping(address => bool) public subscribeState;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -13,17 +16,47 @@ contract Subscribe_yj{
         subscribeState[_user] = _state;
     }
 
-    function subscribe() public payable{
-        require(msg.value >= sub_price, "lack of balance, need to charge");
+    function subscribe1() public payable{
+        require(msg.value >= sub_price1, "lack of balance, need to charge");
+        // require(subscribeState[msg.sender] != false , "already subscribed");
+        payable(address(this)).transfer(msg.value);
+        setSubscribe(msg.sender, true);
+        emit Subscribed(msg.sender, true);
+    }
+
+    function subscribe3() public payable{
+        require(msg.value >= sub_price3, "lack of balance, need to charge");
+        // require(subscribeState[msg.sender] != false , "already subscribed");
+        payable(address(this)).transfer(msg.value);
+        setSubscribe(msg.sender, true);
+        emit Subscribed(msg.sender, true);
+    }
+
+    function subscribe6() public payable{
+        require(msg.value >= sub_price6, "lack of balance, need to charge");
         // require(subscribeState[msg.sender] != false , "already subscribed");
         payable(address(this)).transfer(msg.value);
         setSubscribe(msg.sender, true);
         emit Subscribed(msg.sender, true);
     }
       
-    function cancelSubscribe() public payable{
+    function cancelSubscribe1() public payable{
         require(subscribeState[msg.sender] == true, "not required to cancel");
-        payable(msg.sender).transfer(sub_price);
+        payable(msg.sender).transfer(sub_price1);
+        setSubscribe(msg.sender, false);
+        emit Subscribed(msg.sender, false);
+    }
+
+    function cancelSubscribe3() public payable{
+        require(subscribeState[msg.sender] == true, "not required to cancel");
+        payable(msg.sender).transfer(sub_price3);
+        setSubscribe(msg.sender, false);
+        emit Subscribed(msg.sender, false);
+    }
+
+    function cancelSubscribe6() public payable{
+        require(subscribeState[msg.sender] == true, "not required to cancel");
+        payable(msg.sender).transfer(sub_price6);
         setSubscribe(msg.sender, false);
         emit Subscribed(msg.sender, false);
     }
