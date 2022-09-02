52acf4ea0661a061887ee349b30a75ef81d97151
yjleeinkr <lucy.lee930216@gmail.com>
2022-09-02 14:02:27 +0900

    [back/upload] wip : done for uploading on google storage

--- back/controllers/upload/musicController_.js
+++ back/controllers/upload/musicController_.js
@@ -3,9 +3,30 @@ const storage = new Storage();
 
 const bucketName = 'dappmusic';
 
-const uploadMusic = (req, res) => {
-  const fileName = req.file.filename;
-  console.log(fileName);
+const uploadMusic = async (req, res) => {
+  const fileName = `${req.file.filename}`;
+  let result;
+  try {
+    const filePath = req.file == undefined ? '' : `public/upload/audio/${fileName}`;
+
+    const options = {
+      destination: fileName,
+    };
+
+    await storage.bucket(bucketName).upload(filePath, options);
+    result = {
+      status: true,
+      msg: `${fileName} has been uploaded on ${bucketName}`,
+    };
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+    result = {
+      status: false,
+      msg: `error msg - ${err.msg}`,
+    };
+    res.json(result);
+  }
 };
 
 module.exports = uploadMusic;
