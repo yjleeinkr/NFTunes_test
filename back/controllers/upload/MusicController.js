9145786d47ea112d1cfbed60cff7a252182e1e55
yjleeinkr <lucy.lee930216@gmail.com>
2022-09-05 18:11:25 +0900

    wip: fix multer

--- back/controllers/upload/MusicController.js
+++ back/controllers/upload/MusicController.js
@@ -1,49 +1,32 @@
 const { Storage } = require("@google-cloud/storage");
 const storage = new Storage();
 
-// const uploadMusic = async (req, res) => {
-//   const fileName = `${req.file.filename}`;
-//   let result;
-//   try {
-//     const filePath = req.file == undefined ? '' : `public/upload/audio/${fileName}`;
-
-//     const options = {
-//       destination: fileName,
-//     };
-
-//     await storage.bucket(bucketName).upload(filePath, options);
-//     result = {
-//       status: true,
-//       msg: `${fileName} has been uploaded on ${bucketName}`,
-//     };
-//     res.json(result);
-//   } catch (err) {
-//     console.log(err);
-//     result = {
-//       status: false,
-//       msg: `error msg - ${err.msg}`,
-//     };
-//     res.json(result);
-//   }
-// };
-
 const uploadMusic = async (req, res) => {
-  const storage = new Storage({
-    projectId: proess.env.PROJECT_ID,
-    credentials: {
-      client_email: process.env.CLIENT_EMAIL,
-      private_key: process.env.PRIVATE_KEY,
-    },
-  });
-  const bucket = storage.bucket(process.env.BUCKET_NAME);
-  const file = bucket.file(req.query.file);
-  const options = {
-    expires: Date.now() + 1 * 60 * 1000,
-    fields: { "x-goog-meta-test": "data" },
-  };
+  console.log("메모리스토리지", req.file);
+  const fileName = `${req.file.filename}`;
+  let result;
+  try {
+    const filePath =
+      req.file == undefined ? "" : `public/upload/audio/${fileName}`;
 
-  const [response] = await file.generateSignedPostPolicyV4(options);
-  res.status(200).json(response);
+    const options = {
+      destination: fileName,
+    };
+
+    await storage.bucket(bucketName).upload(filePath, options);
+    result = {
+      status: true,
+      msg: `${fileName} has been uploaded on ${bucketName}`,
+    };
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+    result = {
+      status: false,
+      msg: `error msg - ${err.msg}`,
+    };
+    res.json(result);
+  }
 };
 
 module.exports = uploadMusic;
--- back/utils/multer.js
+++ back/utils/multer.js
@@ -1,33 +1,36 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
-const storage = multer.diskStorage({
-  destination(req, file, done) {
-    const fileType = file.mimetype.split('/')[0];
-    switch (fileType) {
-      case 'audio':
-        done(null, 'public/upload/audio');
-        break;
-      case 'image':
-        done(null, 'public/upload/cover');
-        break;
-      default:
-        done(null, 'public/upload');
-        break;
-    }
-  },
+// const storage = multer.diskStorage({
+//   destination(req, file, done) {
+//     const fileType = file.mimetype.split("/")[0];
+//     switch (fileType) {
+//       case "audio":
+//         done(null, "public/upload/audio");
+//         break;
+//       case "image":
+//         done(null, "public/upload/cover");
+//         break;
+//       default:
+//         done(null, "public/upload");
+//         break;
+//     }
+//   },
 
-  filename(req, file, done) {
-    const ext = path.extname(file.originalname);
-    const fileName = `${path.basename(file.originalname, ext)}_${Date.now()}${ext}`;
-    done(null, fileName);
-  },
-});
+//   filename(req, file, done) {
+//     const ext = path.extname(file.originalname);
+//     const fileName = `${path.basename(
+//       file.originalname,
+//       ext
+//     )}_${Date.now()}${ext}`;
+//     done(null, fileName);
+//   },
+// });
 
 const limits = { fileSize: 10 * 1024 * 1024 };
 
 const multerConfig = {
-  storage,
+  storage: multer.memoryStorage(),
   limits,
 };
 
