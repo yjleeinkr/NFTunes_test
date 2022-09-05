7f3c2319e9f05a1f50dc7514eddb4ef5bf6eaf42
yjleeinkr <lucy.lee930216@gmail.com>
2022-09-05 12:06:25 +0900

    wip

--- back/controllers/upload/MusicController.js
+++ back/controllers/upload/MusicController.js
@@ -1,32 +1,49 @@
-const { Storage } = require('@google-cloud/storage');
+const { Storage } = require("@google-cloud/storage");
 const storage = new Storage();
 
-const bucketName = 'dappmusic';
+// const uploadMusic = async (req, res) => {
+//   const fileName = `${req.file.filename}`;
+//   let result;
+//   try {
+//     const filePath = req.file == undefined ? '' : `public/upload/audio/${fileName}`;
+
+//     const options = {
+//       destination: fileName,
+//     };
+
+//     await storage.bucket(bucketName).upload(filePath, options);
+//     result = {
+//       status: true,
+//       msg: `${fileName} has been uploaded on ${bucketName}`,
+//     };
+//     res.json(result);
+//   } catch (err) {
+//     console.log(err);
+//     result = {
+//       status: false,
+//       msg: `error msg - ${err.msg}`,
+//     };
+//     res.json(result);
+//   }
+// };
 
 const uploadMusic = async (req, res) => {
-  const fileName = `${req.file.filename}`;
-  let result;
-  try {
-    const filePath = req.file == undefined ? '' : `public/upload/audio/${fileName}`;
+  const storage = new Storage({
+    projectId: proess.env.PROJECT_ID,
+    credentials: {
+      client_email: process.env.CLIENT_EMAIL,
+      private_key: process.env.PRIVATE_KEY,
+    },
+  });
+  const bucket = storage.bucket(process.env.BUCKET_NAME);
+  const file = bucket.file(req.query.file);
+  const options = {
+    expires: Date.now() + 1 * 60 * 1000,
+    fields: { "x-goog-meta-test": "data" },
+  };
 
-    const options = {
-      destination: fileName,
-    };
-
-    await storage.bucket(bucketName).upload(filePath, options);
-    result = {
-      status: true,
-      msg: `${fileName} has been uploaded on ${bucketName}`,
-    };
-    res.json(result);
-  } catch (err) {
-    console.log(err);
-    result = {
-      status: false,
-      msg: `error msg - ${err.msg}`,
-    };
-    res.json(result);
-  }
+  const [response] = await file.generateSignedPostPolicyV4(options);
+  res.status(200).json(response);
 };
 
 module.exports = uploadMusic;
--- back/routes/uploadRouter.js
+++ back/routes/uploadRouter.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const thumbnail = require('../controllers/upload/ThumbnailController');
-const uploadMusic = require('../controllers/upload/MusicController');
-const { upload } = require('../utils/multer.js');
+const thumbnail = require("../controllers/upload/ThumbnailController");
+const uploadMusic = require("../controllers/upload/MusicController");
+const { upload } = require("../utils/multer.js");
 
-router.post('/thumbnail', upload.single('cover'), thumbnail);
-router.post('/music', upload.single('music'), uploadMusic);
+router.post("/thumbnail", upload.single("cover"), thumbnail);
+router.post("/music", upload.single("music"), uploadMusic);
 
 module.exports = router;
